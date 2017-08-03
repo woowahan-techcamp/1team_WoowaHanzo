e5735bc3a5bcb5e34d6aedd55c0ccd63f02f49b2
dain7672 <dain7672@gmail.com>
2017-08-03 17:14:26 +0900

    Make new project again

---
+++ iOS/WoowaHanzo_iOS/WoowaHanzo_iOSTests/WoowaHanzo_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  WoowaHanzo_iOSTests.swift
+//  WoowaHanzo_iOSTests
+//
+//  Created by woowabrothers on 2017. 8. 3..
+//  Copyright © 2017년 woowabrothers_dain. All rights reserved.
+//
+
+import XCTest
+@testable import WoowaHanzo_iOS
+
+class WoowaHanzo_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
