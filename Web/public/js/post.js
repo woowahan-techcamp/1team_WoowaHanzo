fbad994f87b71c77806333c3308e9c5a673136f2
jhyang12345 <jhyang12345@naver.com>
2017-08-07 15:33:58 +0900

    Minor updates

--- Web/public/js/post.js
+++ Web/public/js/post.js
@@ -105,5 +105,27 @@ function isLastTaggerEmpty() {
 
 
 function uploadPost() {
-  
+
+}
+
+function writeNewPost(uid, username, picture, title, body) {
+  // A post entry.
+  var postData = {
+    author: username,
+    uid: uid,
+    body: body,
+    title: title,
+    starCount: 0,
+    authorPic: picture
+  };
+
+  // Get a key for a new Post.
+  var newPostKey = firebase.database().ref().child('posts').push().key;
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {};
+  updates['/posts/' + newPostKey] = postData;
+  updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+  return firebase.database().ref().update(updates);
 }
