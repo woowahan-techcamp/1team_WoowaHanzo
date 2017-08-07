076f75ed63beedc5d299b3b14c8469edd1a33831
최혜선 <choehyeseon@pigfig.local>
2017-08-07 14:41:01 +0900

    Create web project folder

---
+++ Web/public/js/autogrow.js
@@ -0,0 +1,67 @@
+(function($){
+
+    $.fn.autoGrowInput = function(o) {
+
+        o = $.extend({
+            maxWidth: 1000,
+            minWidth: 0,
+            comfortZone: 70
+        }, o);
+
+        this.filter('input:text').each(function(){
+
+            var minWidth = o.minWidth || $(this).width(),
+                val = '#',
+                input = $(this),
+                testSubject = $('<tester/>').css({
+                    position: 'absolute',
+                    top: -9999,
+                    left: -9999,
+                    width: 'auto',
+                    fontSize: input.css('fontSize'),
+                    fontFamily: input.css('fontFamily'),
+                    fontWeight: input.css('fontWeight'),
+                    letterSpacing: input.css('letterSpacing'),
+                    whiteSpace: 'nowrap'
+                }),
+                check = function() {
+
+                    if (val === (val = input.val())) {return;}
+
+                    // Enter new content into testSubject
+                    var escaped = val.replace(/&/g, '&amp;').replace(/\s/g,'&nbsp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+                    testSubject.html(escaped);
+                    console.log("autogrow", escaped);
+
+                    // Calculate new width + whether to change
+                    var testerWidth = testSubject.width(),
+                        newWidth = (testerWidth + o.comfortZone) >= minWidth ? testerWidth + o.comfortZone : minWidth,
+                        currentWidth = input.width(),
+                        isValidWidthChange = (newWidth < currentWidth && newWidth >= minWidth)
+                                             || (newWidth > minWidth && newWidth < o.maxWidth);
+
+                    // Animate width
+                    if (isValidWidthChange) {
+                        input.width(newWidth);
+                    }
+
+                };
+
+            testSubject.insertAfter(input);
+
+            $(this).bind('keyup keydown blur update keypress', check);
+
+        });
+
+        return this;
+
+    };
+
+})(jQuery);
+
+/*
+$(function(){
+    $("input.autogrow").autoGrowInput({minWidth:30,comfortZone:30});
+});
+
+*/
