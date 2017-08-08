94d28f0ec6efd574465afb89586c15e60c499b6d
dain7672 <dain7672@gmail.com>
2017-08-08 13:16:53 +0900

    added firebasemodel

---
+++ iOS/WoowaHanzo_iOS/WoowaHanzo_iOS/FirebaseModel.swift
@@ -0,0 +1,11 @@
+//
+//  FirebaseModel.swift
+//  WoowaHanzo_iOS
+//
+//  Created by woowabrothers on 2017. 8. 8..
+//  Copyright © 2017년 woowabrothers_dain. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
