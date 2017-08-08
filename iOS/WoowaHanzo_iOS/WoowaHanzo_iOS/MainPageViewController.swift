8da2d7fdb25f4c068bd6e1f64763ff69e934daae
woowabrothers <ch41150@hanmail.net>
2017-08-08 11:24:12 +0900

    Extension MainPageViewController(UITableView)

--- iOS/WoowaHanzo_iOS/WoowaHanzo_iOS/MainPageViewController.swift
+++ iOS/WoowaHanzo_iOS/WoowaHanzo_iOS/MainPageViewController.swift
@@ -14,13 +14,19 @@ class MainPageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 
+}
+extension MainPageViewController : UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MainPageTableViewCell
+        return cell
+    }
+    
 }
