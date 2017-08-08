a6c8d19d77665e382a6fd413f8515593e346f8bd
jhyang12345 <jhyang12345@naver.com>
2017-08-08 11:32:23 +0900

    Revert "category images uploaded"
    This reverts commit 651f119837033e270f95b4dfeb6ede9484e56daf.

--- Web/public/js/category.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  console.log("category.js started!");
-  console.log($(".category_picker td").width());
-  $(".category_picker td").height($(".category_picker td").width());
-});
