72cea45d3f70e5fa87d1c730ed55d19dc08497f7
woowabrothers <ch41150@hanmail.net>
2017-08-09 09:41:48 +0900

    Add Mainpage collectionView cell

---
+++ iOS/WoowaHanzo_iOS/WoowaHanzo_iOS/MainPageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MainPageCollectionViewCell.swift
+//  WoowaHanzo_iOS
+//
+//  Created by woowabrothers on 2017. 8. 9..
+//  Copyright © 2017년 woowabrothers_dain. All rights reserved.
+//
+
+import UIKit
+
+class MainPageCollectionViewCell: UICollectionViewCell {
+    
+}
