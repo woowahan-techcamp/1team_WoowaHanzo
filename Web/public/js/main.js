8907a10573f8f23568fd639cf6fa3e07806e9e5a
최혜선 <chsun0303@gmail.com>
2017-08-09 09:55:58 +0900

    Add class at signinbtn

--- Web/public/js/main.js
+++ Web/public/js/main.js
@@ -1,7 +1,4 @@
-
 document.addEventListener("DOMContentLoaded", function(event) {
-  console.log("DOM fully loaded and parsed");
-
   var storage = firebase.storage();
 
   // Create a storage reference from our storage service
