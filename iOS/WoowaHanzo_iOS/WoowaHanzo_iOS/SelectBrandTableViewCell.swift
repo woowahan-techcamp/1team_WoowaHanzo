c9854fe151b83138871d82cc8026d0b2e7fe9aa3
woowabrothers <ch41150@hanmail.net>
2017-08-09 16:01:52 +0900

    Add SelectBrandPage Group

---
+++ iOS/WoowaHanzo_iOS/WoowaHanzo_iOS/SelectBrandTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SelectBrandTableViewCell.swift
+//  WoowaHanzo_iOS
+//
+//  Created by woowabrothers on 2017. 8. 9..
+//  Copyright © 2017년 woowabrothers_dain. All rights reserved.
+//
+
+import UIKit
+
+class SelectBrandTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
