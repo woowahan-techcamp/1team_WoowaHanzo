79ac6faac24196abf039177b8df9781c22b1d020
woowabrothers <ch41150@hanmail.net>
2017-08-16 14:20:24 +0900

    Add Login Validation Library

--- iOS/WoowaHanzo_iOS/Pods/Target Support Files/Pods-WoowaHanzo_iOS/Pods-WoowaHanzo_iOS-frameworks.sh
+++ iOS/WoowaHanzo_iOS/Pods/Target Support Files/Pods-WoowaHanzo_iOS/Pods-WoowaHanzo_iOS-frameworks.sh
@@ -91,6 +91,7 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/BSGridCollectionViewLayout/BSGridCollectionViewLayout.framework"
   install_framework "$BUILT_PRODUCTS_DIR/BSImagePicker/BSImagePicker.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ElValidator/ElValidator.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ImageViewer/ImageViewer.framework"
@@ -102,6 +103,7 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/BSGridCollectionViewLayout/BSGridCollectionViewLayout.framework"
   install_framework "$BUILT_PRODUCTS_DIR/BSImagePicker/BSImagePicker.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ElValidator/ElValidator.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ImageViewer/ImageViewer.framework"
