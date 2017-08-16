f02b1d5a2e80ee58780b5877e492dd4463530b07
hyesun03 <chsun0303@gmail.com>
2017-08-16 10:43:56 +0900

    Sign in button changes depending on login status

--- Web/public/js/main.js
+++ Web/public/js/main.js
@@ -4,30 +4,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Create a storage reference from our storage service
   var storageRef = storage.ref();
 
-  if(firebase.auth().currentUser !== null) {
-    $(".nav_signin_btn").html("Sign out");
-  }
-  else {
-    $(".nav_signin_btn").html("Sign in");
-  }
-
   $(".nav_signin_btn").on("click", function() {
-    console.log('clicked..');
-
     if(firebase.auth().currentUser !== null) {
-      console.log('로그인 되어있으니까 로그아웃 ㄱ');
-
       firebase.auth().signOut().then(function() {
-        console.log('로그아웃');
+        $(".nav_signin_btn").html("Sign in");
       }, function(error) {
-        console.log('로그아웃 에러: ', error);
+
       });
     }
     else {
-      console.log('로그인된거 없음 로그인으로 ㄱ');
       window.location.href="login.html";
     }
   });
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      $(".nav_signin_btn").html("Sign out");
+    } else {
+      // No user is signed in.
+      $(".nav_signin_btn").html("Sign in");
+    }
+  });
+
 
 });
 
