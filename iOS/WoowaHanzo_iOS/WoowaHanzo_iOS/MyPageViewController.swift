36d26214e323199e205943a710a151f79a31dcc0
woowabrothers <ch41150@hanmail.net>
2017-08-17 16:04:33 +0900

    Add logout Button

--- iOS/WoowaHanzo_iOS/WoowaHanzo_iOS/MyPageViewController.swift
+++ iOS/WoowaHanzo_iOS/WoowaHanzo_iOS/MyPageViewController.swift
@@ -19,6 +19,15 @@ class MyPageViewController: UIViewController {
         
         // Do any additional setup after loading the view.
     }
+    @IBAction func logout(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         if !AuthModel.isLoginStatus(){
             
