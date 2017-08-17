92147df257f6c48a7800dc3c8d953ee410a623ab
hyesun03 <chsun0303@gmail.com>
2017-08-17 15:05:13 +0900

    Create custom error message at login

--- Web/public/js/login.js
+++ Web/public/js/login.js
@@ -17,15 +17,35 @@ function login() {
   var email = $("#email").val();
   var password = $("#password").val();
 
-  firebase.auth().signInWithEmailAndPassword(email, password)
-  .then(function(confirmationResult) {
-    window.location.href = "index.html";
-  })
-  .catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
+  if(email && password) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function(confirmationResult) {
+      window.location.href = "index.html";
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
 
-    alert(errorCode);
-  });
+      showLoginError(errorCode);
+    });
+  }
+}
+
+function returnCustomLoginErrorMessage(errorCode) {
+  if(errorCode == "auth/invalid-email") {
+    return "이메일 형식이 맞지 않습니다."
+  }
+  else if(errorCode == "auth/user-disabled") {
+    return "사용이 불가능 한 계정입니다."
+  }
+  else if(errorCode == "auth/user-not-found") {
+    return "해당 계정이 존재하지 않습니다."
+  }
+  else if(errorCode == "auth/wrong-password") {
+    return "비밀번호를 확인해 주십시오."
+  }
+  else {
+    return errorCode
+  }
 }
