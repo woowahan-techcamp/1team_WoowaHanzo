cc560a5d13971b5a74cdfbe8bfb7c5c58631a693
hyesun03 <chsun0303@gmail.com>
2017-08-17 14:32:12 +0900

    Press ENTER to login

--- Web/public/js/login.js
+++ Web/public/js/login.js
@@ -2,21 +2,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var signinBtn = $(".signin");
 
   signinBtn.click(function() {
-    var email = $("#email").val();
-    var password = $("#password").val();
-
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(function(confirmationResult) {
-      window.location.href = "index.html";
-    })
-    .catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-
-      alert(errorCode);
-    });
+    login();
   });
 
-
+  $("body").on("keypress", function(evt) {
+    var keycode = evt.keyCode || evt.which;
+    if(keycode == 13) {
+      login();
+    }
+  });
 });
+
+function login() {
+  var email = $("#email").val();
+  var password = $("#password").val();
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then(function(confirmationResult) {
+    window.location.href = "index.html";
+  })
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    alert(errorCode);
+  });
+}
