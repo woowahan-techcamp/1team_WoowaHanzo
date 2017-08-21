220408db92c1d8817a8272d71ab653887dfa6e18
woowabrothers <ch41150@hanmail.net>
2017-08-21 21:02:24 +0900

    Implemented to access the corresponding feed with key in tag array

--- iOS/WoowaHanzo_iOS/WoowaHanzo_iOS/FirebaseModel.swift
+++ iOS/WoowaHanzo_iOS/WoowaHanzo_iOS/FirebaseModel.swift
@@ -98,4 +98,29 @@ class FirebaseModel{
         
             
     }
+    func loadFeedTag(){
+        
+        self.ref = Database.database().reference().child("posts")
+        
+        self.ref.queryOrdered(byChild: "time").observeSingleEvent(of: .value, with: { (snapshot) in
+            if let result = snapshot.children.allObjects as? [DataSnapshot]{
+                User.users = [User]()
+                for child in result {
+                    //print(child)
+                    var userKey = child.key as! String
+                    //print(child.childSnapshot(forPath: "author").value!)
+                    let user = User(key: userKey, nickName: child.childSnapshot(forPath: "author").value as! String, contents: child.childSnapshot(forPath: "body").value as! String,tags: child.childSnapshot(forPath: "tags").value as? [String] ?? nil,imageArray:child.childSnapshot(forPath: "images").value as? [String] ?? nil, postDate : child.childSnapshot(forPath: "time").value as! Int)
+                    User.users.append(user)
+                    
+                    
+                    NotificationCenter.default.post(name: NSNotification.Name(rawValue: "reload"), object: nil)
+                }
+                
+                
+                
+            }
+        })
+        
+
+    }
 }
--- iOS/WoowaHanzo_iOS/WoowaHanzo_iOS/TagResultTableViewController.swift
+++ iOS/WoowaHanzo_iOS/WoowaHanzo_iOS/TagResultTableViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import Firebase
 
 class TagResultTableViewController: UITableViewController {
+    
+    var ref: DatabaseReference!
 
     var tagName:String = ""
     var tagFeedArray = [Any]()
@@ -21,20 +24,17 @@ class TagResultTableViewController: UITableViewController {
         self.navigationController?.navigationBar.topItem?.title = "태그"
        //self.title = tagName
         self.navigationItem.title = tagName
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        
+        
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
@@ -44,16 +44,26 @@ class TagResultTableViewController: UITableViewController {
     func getTagFeed(_ notification: Notification){
         tagFeedArray = notification.userInfo?["tagResultArray"] as! [Any]
         print(tagFeedArray)
+        self.ref = Database.database().reference().child("posts")
+        for index in 1..<tagFeedArray.count{
+            
+            let refHandle = ref.observe(DataEventType.value, with: { (snapshot) in
+                if let result = snapshot.childSnapshot(forPath: self.tagFeedArray[index] as! String).value {
+                    print(result)
+                }
+                
+                    
+            })
+
+        }
     }
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TagResultTableViewCell
+        cell.tagResultNickNameLabel.text = "dd"
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
