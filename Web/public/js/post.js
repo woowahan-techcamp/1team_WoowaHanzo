d35b318e37826026a4b8b55390e2a02064a9281c
hyesun03 <chsun0303@gmail.com>
2017-08-21 16:27:16 +0900

    Add loading indicator at post

--- Web/public/js/post.js
+++ Web/public/js/post.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         curObject.uid = user.uid;
       	curObject.queryClass = "profilePic";
       	loadUserProfile.bind(curObject, user.uid)();
+
       });
     }
   });
