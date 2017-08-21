512a05e2a395b732166f89f8694095e135c2eafb
woowabrothers <ch41150@hanmail.net>
2017-08-21 15:40:29 +0900

    register Page view can send register info

--- iOS/WoowaHanzo_iOS/WoowaHanzo_iOS/StringExtension.swift
+++ iOS/WoowaHanzo_iOS/WoowaHanzo_iOS/StringExtension.swift
@@ -1,3 +1,6 @@
+import UIKit
+import Foundation
+
 extension String {
     subscript (i: Int) -> Character {
         return self[self.characters.index(self.startIndex, offsetBy: i)]
@@ -17,5 +20,11 @@ extension String {
         let start = index(startIndex, offsetBy: r.lowerBound)
         let end = index(startIndex, offsetBy: r.upperBound)
         return self[start...end]
+    }
+    
+    func isValidEmail() -> Bool {
+        // here, `try!` will always succeed because the pattern is valid
+        let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .caseInsensitive)
+        return regex.firstMatch(in: self, options: [], range: NSRange(location: 0, length: characters.count)) != nil
     }
 }
