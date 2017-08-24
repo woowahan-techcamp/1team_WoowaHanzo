4a31944e1b216619664d02453f20f893c55cdcc3
dain7672 <dain7672@gmail.com>
2017-08-24 15:07:24 +0900

    minor error

--- iOS/WoowaHanzo_iOS/WoowaHanzo_iOS/MyPageViewController.swift
+++ iOS/WoowaHanzo_iOS/WoowaHanzo_iOS/MyPageViewController.swift
@@ -13,7 +13,7 @@ import Kingfisher
 class MyPageViewController: UIViewController {
     
     var ref: DatabaseReference!
-
+    
     
     var postProfileImageName:String = ""
     
@@ -39,7 +39,7 @@ class MyPageViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(viewload), name: NSNotification.Name(rawValue: "users3"), object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(updateProfileImg), name: NSNotification.Name(rawValue: "profileimg"), object: nil)
         
-
+        
         //myListView = UserListView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
         //myListView.ypos = 250
         //myInfoView.frame = CGRect(x: 0, y: 5, width: self.view.frame.width, height: 250)
@@ -54,7 +54,7 @@ class MyPageViewController: UIViewController {
         if AuthModel.isLoginStatus(){
             print("logined")
             self.navigationController?.navigationBar.topItem?.title = User.currentLoginedUserNickName
-
+            
         }
         else {
             self.navigationController?.navigationBar.topItem?.title = "마이페이지"
@@ -71,118 +71,122 @@ class MyPageViewController: UIViewController {
         
         
     }
-    func loadUserInfo(){
-        self.navigationController?.navigationBar.topItem?.title = User.currentLoginedUserNickName
-        print("loadUserInfo")
+//    func loadUserInfo(){
+//        self.navigationController?.navigationBar.topItem?.title = User.currentLoginedUserNickName
+//        print("loadUserInfo")
+//    }
     
-    func viewload(){
-        self.navigationController?.navigationBar.topItem?.title =  User.currentLoginedUserNickName
-        nameLabel.text = User.currentLoginedUserNickName
-        nameLabel.sizeToFit()
-        print("\(User.myUsers.count)개의 랭크 데이터가 존재합니다.")
-        myListView.addUserList(users: User.myUsers)
-        postnumLabel.text = "게시물 \(User.myUsers.count)"
-        postnumLabel.sizeToFit()
-        postnumLabel.frame.origin.x = self.view.frame.width / 2 - postnumLabel.frame.width / 2
-        logoutButton.tintColor = UIColor.blue
-
-    }
-    func loadUserInfo(){
-        viewload()
-        FirebaseModel().loadProfileImageFromUsers() // 나중에 바꾸기.
-        print("loadUserInfo/////////////////////////")
-
-    }
-    func loadPorfileImage(_ notification : Notification){
-        let profileImageUrl = notification.userInfo?["profileImageUrl"] as! String
-        Storage.storage().reference(withPath: "profileImages/" + profileImageUrl).downloadURL { (url, error) in
-            self.myProfileImageView?.kf.setImage(with: url)
-            //completion handler 등으로 user에 저장해놓기
-        }
-    }
-    @IBAction func logout(_ sender: Any) {
-        let firebaseAuth = Auth.auth()
-        do {
-            try firebaseAuth.signOut()
-            let storyboard = UIStoryboard(name: "MainLayout", bundle: nil)
-            let controller = storyboard.instantiateViewController(withIdentifier: "mainLayout")
-            self.present(controller, animated: false, completion: nil)
-        } catch let signOutError as NSError {
-            print ("Error signing out: %@", signOutError)
-        }
-        
-    }
-    
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        
-        
-        if !AuthModel.isLoginStatus(){
-            
-            var alert = UIAlertController(title: "로그인 후 이용하실 수 있습니다. ", message: "로그인 하시겠습니까?", preferredStyle: .alert)
-            var cancel = UIAlertAction(title: "cancel", style: .cancel, handler: { (cancelAction) in
-//                let storyboard = UIStoryboard(name: "MainLayout", bundle: nil)
-//                let controller = storyboard.instantiateViewController(withIdentifier: "mainLayout")
-//                self.present(controller, animated: false, completion: nil)
-                //self.navigationController?.pushViewController(controller, animated: true)
-                //self.show(controller, sender: self)
-                self.tabBarController?.selectedIndex = 0
-            })
-            var ok = UIAlertAction(title: "OK", style: .default, handler: { (okAction) in
-                let storyboard = UIStoryboard(name: "Auth", bundle: nil)
-                let controller = storyboard.instantiateViewController(withIdentifier: "loginNavigation")
-                self.show(controller, sender: self)
-
-            })
-            alert.addAction(cancel)
-            alert.addAction(ok)
-            present(alert, animated: true, completion: nil)
-        }
-        else{
-            //로그인이 되었다면? 내 마이페이지를 보여줘야함.
+        func viewload(){
             self.navigationController?.navigationBar.topItem?.title =  User.currentLoginedUserNickName
-            self.view.setNeedsDisplay()
+            nameLabel.text = User.currentLoginedUserNickName
+            nameLabel.sizeToFit()
+            print("\(User.myUsers.count)개의 랭크 데이터가 존재합니다.")
+            myListView.addUserList(users: User.myUsers)
+            postnumLabel.text = "게시물 \(User.myUsers.count)"
+            postnumLabel.sizeToFit()
+            postnumLabel.frame.origin.x = self.view.frame.width / 2 - postnumLabel.frame.width / 2
+            logoutButton.tintColor = UIColor.blue
+            
         }
-            myListView = UserListView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
-            myListView.ypos = 313
-            myInfoView.frame = CGRect(x: 0, y: 68, width: self.view.frame.width, height: 250)
-            myListView.addSubview(myInfoView)
-            
-            myProfileImageView.layer.cornerRadius = myProfileImageView.frame.width / 2
-            myProfileImageView.clipsToBounds = true
-            
-            FirebaseModel().loadProfileImageFromUsers()
-            FirebaseModel().loadUserInfo()
-            FirebaseModel().loadUsers3(username: UserDefaults.standard.string(forKey: "userNickName")!)
-            self.view.addSubview(myListView)
-            }
         
-    }
-    func imageViewTouched(){
-        print("touched")
-    }
-    
-    func updateProfileImg(_ notification: Notification){
-        let profileimg = notification.userInfo?["profileimg"] as? String ?? nil
-        let imageview = notification.userInfo?["imgview"] as? UIImageView ?? nil
-        let ranknamelabel = notification.userInfo?["ranklabel"] as? UILabel ?? nil
-        let rankname = notification.userInfo?["rankname"] as? String ?? ""
-        if profileimg != nil && imageview != nil {
-            Storage.storage().reference(withPath: "profileImages/" + profileimg!).downloadURL { (url, error) in
-                imageview?.kf.setImage(with: url)
+        
+        
+        func loadUserInfo(){
+            viewload()
+            FirebaseModel().loadProfileImageFromUsers() // 나중에 바꾸기.
+            print("loadUserInfo/////////////////////////")
+            
+        }
+        
+        func loadPorfileImage(_ notification : Notification){
+            let profileImageUrl = notification.userInfo?["profileImageUrl"] as! String
+            Storage.storage().reference(withPath: "profileImages/" + profileImageUrl).downloadURL { (url, error) in
+                self.myProfileImageView?.kf.setImage(with: url)
+                //completion handler 등으로 user에 저장해놓기
             }
         }
-        if ranknamelabel != nil {
-            ranknamelabel?.text = rankname
-            ranknamelabel?.sizeToFit()
+        @IBAction func logout(_ sender: Any) {
+            let firebaseAuth = Auth.auth()
+            do {
+                try firebaseAuth.signOut()
+                let storyboard = UIStoryboard(name: "MainLayout", bundle: nil)
+                let controller = storyboard.instantiateViewController(withIdentifier: "mainLayout")
+                self.present(controller, animated: false, completion: nil)
+            } catch let signOutError as NSError {
+                print ("Error signing out: %@", signOutError)
+            }
+            
         }
-    }
-
-
-
-
-
+        
+        
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(true)
+            
+            
+            if !AuthModel.isLoginStatus(){
+                
+                var alert = UIAlertController(title: "로그인 후 이용하실 수 있습니다. ", message: "로그인 하시겠습니까?", preferredStyle: .alert)
+                var cancel = UIAlertAction(title: "cancel", style: .cancel, handler: { (cancelAction) in
+                    //                let storyboard = UIStoryboard(name: "MainLayout", bundle: nil)
+                    //                let controller = storyboard.instantiateViewController(withIdentifier: "mainLayout")
+                    //                self.present(controller, animated: false, completion: nil)
+                    //self.navigationController?.pushViewController(controller, animated: true)
+                    //self.show(controller, sender: self)
+                    self.tabBarController?.selectedIndex = 0
+                })
+                var ok = UIAlertAction(title: "OK", style: .default, handler: { (okAction) in
+                    let storyboard = UIStoryboard(name: "Auth", bundle: nil)
+                    let controller = storyboard.instantiateViewController(withIdentifier: "loginNavigation")
+                    self.show(controller, sender: self)
+                    
+                })
+                alert.addAction(cancel)
+                alert.addAction(ok)
+                present(alert, animated: true, completion: nil)
+            }
+            else{
+                //로그인이 되었다면? 내 마이페이지를 보여줘야함.
+                self.navigationController?.navigationBar.topItem?.title =  User.currentLoginedUserNickName
+                self.view.setNeedsDisplay()
+                myListView = UserListView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+                myListView.ypos = 313
+                myInfoView.frame = CGRect(x: 0, y: 68, width: self.view.frame.width, height: 250)
+                myListView.addSubview(myInfoView)
+                
+                myProfileImageView.layer.cornerRadius = myProfileImageView.frame.width / 2
+                myProfileImageView.clipsToBounds = true
+                
+                FirebaseModel().loadProfileImageFromUsers()
+                FirebaseModel().loadUserInfo()
+                FirebaseModel().loadUsers3(username: UserDefaults.standard.string(forKey: "userNickName")!)
+                self.view.addSubview(myListView)
+            }
+            
+        }
+        func imageViewTouched(){
+            print("touched")
+        }
+        
+        func updateProfileImg(_ notification: Notification){
+            let profileimg = notification.userInfo?["profileimg"] as? String ?? nil
+            let imageview = notification.userInfo?["imgview"] as? UIImageView ?? nil
+            let ranknamelabel = notification.userInfo?["ranklabel"] as? UILabel ?? nil
+            let rankname = notification.userInfo?["rankname"] as? String ?? ""
+            if profileimg != nil && imageview != nil {
+                Storage.storage().reference(withPath: "profileImages/" + profileimg!).downloadURL { (url, error) in
+                    imageview?.kf.setImage(with: url)
+                }
+            }
+            if ranknamelabel != nil {
+                ranknamelabel?.text = rankname
+                ranknamelabel?.sizeToFit()
+            }
+        }
+        
+        
+        
+        
+        
 }
 
 
