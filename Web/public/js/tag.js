c1c2ef5aaafb73b62b7a4ea1f27046070626660a
hyesun03 <chsun0303@gmail.com>
2017-08-24 09:02:40 +0900

    Show tags sorted by time

--- Web/public/js/tag.js
+++ Web/public/js/tag.js
@@ -1,20 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-
-  var tagList;
+  var tagList = [];
+  var tagNameList = [];
+  var tagTimeList = [];
   var promises = [];
-  promises.push(firebase.database().ref("tagCounter/").once("value").then(function(snapshots) {
-    tagList = Object.keys(snapshots.val());
-  }));
+  firebase.database().ref("tagCounter/").orderByChild("time").once("value").then(function(snapshots) {
+    snapshots.forEach(function(child) {
+      tagNameList.push(child.key);
+      tagTimeList.push(getCurrentTime(child.val().time));
+    });
 
-  Promise.all(promises).then(() => {
-    console.log("tags: ", tagList);
+    tagNameList.reverse();
+    tagTimeList.reverse();
+
+    tagList = new Array(tagNameList.length);
+
+    // console.log('tagTimeList', tagTimeList);
+    // console.log('tagNameList', tagNameList);
+
+    for(var i = 0; i < tagNameList.length; i++) {
+      var tmpObject = {};
+      tmpObject.tagTime = tagTimeList[i];
+      tmpObject.tagName = tagNameList[i];
+      tagList[i] = tmpObject;
+    }
+
     var source = document.getElementById("tag_template").innerHTML;
     var template = Handlebars.compile(source);
     document.querySelector(".tags_holder").innerHTML += template(tagList);
   });
-
-
-
-
 
 });
